663ce3af72c72313a30a8bf3ad895a6f074189a9
alviramadhan <alviramadhan77@gmail.com>
2025-03-29 22:23:35 +1100

    connect reminder form to db

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,3 +6,24 @@ const client = new Client()
 
 export const account = new Account(client);
 export const databases = new Databases(client);
+
+import { ID } from "react-native-appwrite";
+
+const DATABASE_ID = '67dc12010001c35043dd'; // replace this with your actual database ID
+const REMINDER_COLLECTION_ID = '67e56e4b00320f59c79d'; // replace this with your actual collection ID
+
+export async function createReminder(data: {
+  title: string;
+  notes: string;
+  dateTime: string;
+  type: string;
+  userId: string;
+  petId: string;
+}) {
+  return await databases.createDocument(
+    DATABASE_ID,
+    REMINDER_COLLECTION_ID,
+    ID.unique(),
+    data
+  );
+}
